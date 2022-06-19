c1633602ed03f230ddd3dc86c944281ed25670f4
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-06-19 10:45:19 +0200

    wip refactor

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,11 +10,13 @@ fromNumber 50 = "L"
 fromNumber 100 = "C"
 fromNumber 500 = "D"
 fromNumber 1000 = "M"
-fromNumber 4 = "IV"
-fromNumber 6 = "VI"
-fromNumber 7 = "VII"
-fromNumber 8 = "VIII"
-fromNumber 9 = "IV"
+
+fromNumber 6 = fromNumber 5 ++ fromNumber 1
+fromNumber 7 = fromNumber 5 ++ fromNumber 2
+fromNumber 8 = fromNumber 5 ++ fromNumber 3
+
+fromNumber 4 = fromNumber 1 ++ fromNumber 5
+fromNumber 9 = fromNumber 1 ++ fromNumber 10
 
 fromNumber number = take number $ repeat 'I'
 
