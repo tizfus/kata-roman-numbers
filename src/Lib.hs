bbffae1c1bad4b7f81122b4476c8392503658d18
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-06-21 17:49:54 +0200

    use strings instead of chars

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,46 +3,46 @@ module Lib
     ) where
 
 fromNumber :: Int -> String
-fromNumber 1 = [drawI]
-fromNumber 5 = [drawV]
-fromNumber 10 = [drawX]
-fromNumber 50 = [drawL]
-fromNumber 100 = [drawC]
-fromNumber 500 = [drawD]
-fromNumber 1000 = [drawM]
+fromNumber 1 = drawI
+fromNumber 5 = drawV
+fromNumber 10 = drawX
+fromNumber 50 = drawL
+fromNumber 100 = drawC
+fromNumber 500 = drawD
+fromNumber 1000 = drawM
 
-fromNumber 4 = [drawI, drawV]
-fromNumber 40 = [drawI, drawL]
-fromNumber 400 = [drawI, drawD]
+fromNumber 4 = drawI ++ drawV
+fromNumber 40 = drawI ++ drawL
+fromNumber 400 = drawI ++ drawD
 fromNumber 4000 = drawMs 4
 
-fromNumber 9 = [drawI , drawX]
-fromNumber 90 = [drawI , drawC]
-fromNumber 900 = [drawI , drawM]
+fromNumber 9 = drawI ++ drawX
+fromNumber 90 = drawI ++ drawC
+fromNumber 900 = drawI ++ drawM
 
 fromNumber number
-    | (number > 10) = drawX : (fromNumber $ number - 10)
-    | (number > 5) = drawV : drawIs (number - 5)
+    | (number > 10) = drawX ++ (fromNumber $ number - 10)
+    | (number > 5) = drawV ++ drawIs (number - 5)
     | (number > 1) = drawIs number
     
     | otherwise = "WRONG_VALUE"
 
-drawI = 'I'
+drawI = "I"
 drawIs = draws drawI
 
-drawV = 'V'
+drawV = "V"
 
-drawX = 'X'
+drawX = "X"
 drawXs = draws drawX
 
-drawL = 'L'
+drawL = "L"
 
-drawD = 'D'
+drawD = "D"
 
-drawC = 'C'
+drawC = "C"
 
-drawM = 'M'
+drawM = "M"
 drawMs = draws drawM
 
-draws :: Char -> Int -> String 
-draws letter amount = take amount $ repeat letter 
+draws :: String -> Int -> String 
+draws letter amount = concat $ take amount $ repeat letter 
