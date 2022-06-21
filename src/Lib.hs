59e863bb78d43ad1c7a2974ccf794aba4dc0cf6e
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-06-21 23:57:32 +0200

    group roman symbols and sort them

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,9 @@ module Lib
     ( fromNumber
     ) where
 
+import Data.List (sortBy)
+import Data.Function (on)
+
 fromNumber :: Int -> String
 fromNumber number =
     let 
@@ -20,19 +23,28 @@ isZero :: Int -> Bool
 isZero = (== 0)
 
 romanNumbers :: [(String, Int)]
-romanNumbers = [
-    ("MMMM", 4000),
-    ("M", 1000),
-    ("CM", 900),
-    ("D", 500),
-    ("CD", 400),
-    ("C", 100),
-    ("XC", 90),
-    ("L", 50),
-    ("XL", 40),
-    ("X", 10),
-    ("IX", 9),
-    ("V", 5),
-    ("IV", 4),
-    ("I", 1)
-    ]
+romanNumbers = 
+    sort $ symbols ++ specialNumbers
+    where 
+        symbols = [
+            ("MMMM", 4000),
+            ("M", 1000),
+            ("D", 500),
+            ("C", 100),
+            ("L", 50),
+            ("X", 10),
+            ("V", 5),
+            ("I", 1)
+            ]
+
+        specialNumbers = [
+            ("CM", 900),
+            ("CD", 400),
+            ("XC", 90),
+            ("XL", 40),
+            ("IX", 9),
+            ("IV", 4)
+            ]
+
+sort :: [(String, Int)] -> [(String, Int)]
+sort = sortBy (flip compare `on` snd)
